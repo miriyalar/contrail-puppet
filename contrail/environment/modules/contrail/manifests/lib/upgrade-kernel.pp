ebee27f5e5f730d772c98f709d93c18759832372
tsurendra <tsurendra@juniper.net>
2015-07-22 11:36:37 -0700

    Closes-Bug: #1470628 Server Manager: Use puppet labs reboot instead of reboot command
    Fix:
        Notify the reboot resourse.

--- contrail/environment/modules/contrail/manifests/lib/upgrade-kernel.pp
+++ contrail/environment/modules/contrail/manifests/lib/upgrade-kernel.pp
@@ -18,19 +18,20 @@ define contrail::lib::upgrade-kernel(
             $image = "linux-image-${contrail_dist_kernel_version}-generic"
             $image_extra = "linux-image-extra-${contrail_dist_kernel_version}-generic"
 
-            package { $headers : ensure => present, }
+            package { $headers : ensure => present, notify => Reboot["after"],  }
             ->
-            package { $headers_generic : ensure => present, }
+            package { $headers_generic : ensure => present, notify => Reboot["after"],  }
             ->
-            package { $image : ensure => present, }
+            package { $image : ensure => present, notify => Reboot["after"],  }
             ->
-            package { $image_extra : ensure => present, }
+            package { $image_extra : ensure => present, notify => Reboot["after"],  }
             ->
-	    reboot { 'after':
-	      apply => "immediately",
-	      timeout => 0,
-	      subscribe       => Package[$image_extra],
-	    }
+            reboot { 'after':
+              apply => "immediately",
+              timeout => 0,
+              message => "Rebooting for kernel upgrade",
+              subscribe       => Package[$image_extra],
+            }
         } else {
             if ($contrail_kernel_version == "" ) {
                 $contrail_dist_kernel_version = "3.13.0-34"
@@ -43,19 +44,20 @@ define contrail::lib::upgrade-kernel(
             $image = "linux-image-${contrail_dist_kernel_version}-generic"
             $image_extra = "linux-image-extra-${contrail_dist_kernel_version}-generic"
 
-            package { 'apparmor' : ensure => '2.7.102-0ubuntu3.10',}
+            package { 'apparmor' : ensure => '2.7.102-0ubuntu3.10', notify => Reboot["after"], }
             ->
-            package { $headers : ensure => present, }
+            package { $headers : ensure => present, notify => Reboot["after"], }
             ->
-            package { $headers_generic : ensure => present, }
+            package { $headers_generic : ensure => present, notify => Reboot["after"], }
             ->
-            package { $image : ensure => present, }
+            package { $image : ensure => present, notify => Reboot["after"], }
             ->
-	    reboot { 'after':
-	      subscribe       => Package[$image],
-	      apply => "immediately",
-	      timeout => 0,
-	    }
+            reboot { 'after':
+              subscribe       => Package[$image],
+              apply => "immediately",
+              message => "Rebooting for kernel upgrade",
+              timeout => 0,
+            }
         }
     } else {
         #TODO for other flavours do nothing
